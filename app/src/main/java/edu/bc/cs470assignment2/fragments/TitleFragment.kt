c52b35fbbbe6bc675c84e16680b0de0bd9e3a706
Andrew Gregersen <andrew.gregersen@bellevuecollege.edu>
2021-05-16 10:23:55 -0700

    Created all needed fragments, nothing on them yet

---
+++ app/src/main/java/edu/bc/cs470assignment2/fragments/TitleFragment.kt
@@ -0,0 +1,21 @@
+package edu.bc.cs470assignment2.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class TitleFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+    }
+
+
+}
