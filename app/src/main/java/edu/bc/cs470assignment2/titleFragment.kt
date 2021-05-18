305797c5a8a2b95b4b5f2077e03f351974f961d1
Andrew Gregersen <andrew.gregersen@bellevuecollege.edu>
2021-05-17 17:05:05 -0700

    Started inplementing the rest of everything else

--- app/src/main/java/edu/bc/cs470assignment2/titleFragment.kt
+++ app/src/main/java/edu/bc/cs470assignment2/titleFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import edu.bc.cs470assignment2.databinding.FragmentTitleBinding
 
 
 class titleFragment : Fragment() {
@@ -15,7 +18,19 @@ class titleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false)
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(
+            inflater,
+            R.layout.fragment_title,
+            container,
+            false
+        )
+
+        binding.button2.setOnClickListener {
+            this.findNavController()
+                .navigate(titleFragmentDirections.actionTitleFragmentToFirstFragment())
+        }
+
+        return binding.root
     }
-    
+
 }
